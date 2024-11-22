112626492d3661d9d30d31b7ce04128ff93142ef
tiffanyiong <tiffanyiong924@gmail.com>
2024-11-22 15:41:10 -0500

    LinkedList

---
+++ Coding/src/list/LinkedList.java
@@ -0,0 +1,35 @@
+package list;
+
+public class LinkedList {
+
+    public ListNode reverseLinkedList(ListNode head) {
+        ListNode pre = null;
+        ListNode next;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(ListNode next) {
+        this.next = next;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
