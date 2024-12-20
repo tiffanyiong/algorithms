68f7b6721b366f23f8d2a19f1205bcbd87884a16
tiffanyiong <tiffanyiong924@gmail.com>
2024-12-20 17:32:52 -0500

    2208. Minimum Operations to Halve Array Sum

--- Coding/src/heap/MinOperationsToHalveArraySum2208.java
+++ Coding/src/heap/MinOperationsToHalveArraySum2208.java
@@ -4,6 +4,12 @@ import java.util.PriorityQueue;
 
 /**
  * https://leetcode.cn/problems/minimum-operations-to-halve-array-sum/description/
+ *
+ * You are given an array nums of positive integers.
+ * In one operation, you can choose any number from nums and reduce it to exactly half the number.
+ * (Note that you may choose this reduced number in future operations.)
+ *
+ * Return the minimum number of operations to reduce the sum of nums by <b>at least</b> half.
  */
 public class MinOperationsToHalveArraySum2208 {
 
